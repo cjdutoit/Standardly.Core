371687e99dd0e2814bdac64305994bd671060557
Christo du Toit <christo@dutoit.co.uk>
2022-10-27 14:41:37 +0100

    ShouldThrowValidationExceptionOnRunIfExecutionsIsNull -> FAIL

--- Standardly.Core.Tests.Unit/Services/Foundations/Executions/ExecutionServiceTests.Validations.RunScript.cs
+++ Standardly.Core.Tests.Unit/Services/Foundations/Executions/ExecutionServiceTests.Validations.RunScript.cs
@@ -20,11 +20,15 @@ namespace Standardly.Core.Tests.Unit.Services.Foundations.Executions
         [InlineData(null)]
         [InlineData("")]
         [InlineData("   ")]
-        public async Task ShouldThrowValidationExceptionOnRunIfExecutionFolderIsInvalid(string invalidExecutionFolder)
+        public async Task ShouldThrowValidationExceptionOnRunIfExecutionFolderIsInvalid(string invalidValue)
         {
             // given
-            string inputExecutionFolder = invalidExecutionFolder;
-            List<Execution> someExecutions = GetRandomExecutions();
+            string inputExecutionFolder = invalidValue;
+
+            List<Execution> someExecutions = new List<Execution>()
+            {
+                new Execution(name: invalidValue, instruction: invalidValue)
+            };
 
             var invalidArgumentExecutionException =
                 new InvalidArgumentExecutionException();
@@ -32,6 +36,44 @@ namespace Standardly.Core.Tests.Unit.Services.Foundations.Executions
             invalidArgumentExecutionException.AddData(
                 key: "executionFolder",
                 values: "Text is required");
+
+            invalidArgumentExecutionException.AddData(
+                key: "executions",
+                values: "Executions is required");
+
+            var expectedExecutionValidationException =
+                new ExecutionValidationException(invalidArgumentExecutionException);
+
+            // when
+            ValueTask<string> runTask = this.executionService.Run(someExecutions, inputExecutionFolder);
+
+            ExecutionValidationException actualExecutionValidationException =
+                await Assert.ThrowsAsync<ExecutionValidationException>(runTask.AsTask);
+
+            // then
+            actualExecutionValidationException.Should().BeEquivalentTo(expectedExecutionValidationException);
+
+            this.executionBrokerMock.Verify(broker =>
+                broker.Run(someExecutions, inputExecutionFolder),
+                    Times.Never);
+
+            this.executionBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRunIfExecutionsIsNull()
+        {
+            // given
+            string randomExecutionFolder = GetRandomString();
+            string inputExecutionFolder = randomExecutionFolder;
+            List<Execution> someExecutions = null;
+
+            var invalidArgumentExecutionException =
+                new InvalidArgumentExecutionException();
+
+            invalidArgumentExecutionException.AddData(
+                key: "executions",
+                values: "Executions is required");
 
             var expectedExecutionValidationException =
                 new ExecutionValidationException(invalidArgumentExecutionException);
