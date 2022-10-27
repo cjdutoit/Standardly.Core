18f6d5c4dbc0a1dc2f0be46681231e83e8216b7b
Christo du Toit <christo@dutoit.co.uk>
2022-10-27 18:11:50 +0100

    ShouldRunExecution -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Processings/Executions/ExecutionProcessingServiceTests.cs
@@ -0,0 +1,51 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using Moq;
+using Standardly.Core.Brokers.Loggings;
+using Standardly.Core.Models.Foundations.Executions;
+using Standardly.Core.Services.Foundations.Executions;
+using Standardly.Core.Services.Processings.Executions;
+using Tynamix.ObjectFiller;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.Executions
+{
+    public partial class ExecutionProcessingServiceTests
+    {
+        private readonly Mock<IExecutionService> executionServiceMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IExecutionProcessingService executionProcessingService;
+
+        public ExecutionProcessingServiceTests()
+        {
+            this.executionServiceMock = new Mock<IExecutionService>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.executionProcessingService = new ExecutionProcessingService(
+                executionService: this.executionServiceMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 5).GetValue();
+
+        private static List<Execution> GetRandomExecutions()
+        {
+            List<Execution> executions = new List<Execution>();
+
+            for (int i = 0; i < GetRandomNumber(); i++)
+            {
+                executions.Add(new Execution(name: GetRandomString(), instruction: GetRandomString()));
+            }
+
+            return executions;
+        }
+    }
+}
---
+++ Standardly.Core/Services/Processings/Executions/ExecutionProcessingService.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Standardly.Core.Brokers.Loggings;
+using Standardly.Core.Models.Foundations.Executions;
+using Standardly.Core.Services.Foundations.Executions;
+
+namespace Standardly.Core.Services.Processings.Executions
+{
+    public class ExecutionProcessingService : IExecutionProcessingService
+    {
+        private readonly IExecutionService executionService;
+        private readonly ILoggingBroker loggingBroker;
+
+        public ExecutionProcessingService(IExecutionService executionService, ILoggingBroker loggingBroker)
+        {
+            this.executionService = executionService;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<string> Run(List<Execution> executions, string executionFolder) =>
+            throw new NotImplementedException();
+    }
+}
