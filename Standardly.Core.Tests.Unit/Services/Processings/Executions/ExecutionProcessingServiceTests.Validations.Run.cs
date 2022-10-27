168a76dafb858ee92e71ca427d705bf9a8de58bc
Christo du Toit <christo@dutoit.co.uk>
2022-10-27 18:31:07 +0100

    ShouldThrowValidationExceptionOnRunIfExecutionsIsNullAndLogItAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Processings/Executions/ExecutionProcessingServiceTests.Validations.Run.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Foundations.Executions;
+using Standardly.Core.Models.Processings.Executions.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.Executions
+{
+    public partial class ExecutionProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRunIfExecutionsIsNullAndLogItAsync()
+        {
+            // given
+            List<Execution> nullExecutions = null;
+            string executionFolder = GetRandomString();
+
+            var nullExecutionProcessingException =
+                new NullExecutionProcessingException();
+
+            var expectedExecutionProcessingValidationException =
+                new ExecutionProcessingValidationException(nullExecutionProcessingException);
+
+            // when
+            ValueTask<string> runTask =
+                this.executionProcessingService.Run(nullExecutions, executionFolder);
+
+            ExecutionProcessingValidationException actualExecutionProcessingValidationException =
+                await Assert.ThrowsAsync<ExecutionProcessingValidationException>(runTask.AsTask);
+
+            // then
+            actualExecutionProcessingValidationException.Should()
+                .BeEquivalentTo(expectedExecutionProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedExecutionProcessingValidationException))),
+                        Times.Once);
+
+            this.executionServiceMock.Verify(broker =>
+                broker.Run(nullExecutions, executionFolder),
+                    Times.Never);
+
+            this.executionServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- Standardly.Core.Tests.Unit/Services/Processings/Executions/ExecutionProcessingServiceTests.cs
+++ Standardly.Core.Tests.Unit/Services/Processings/Executions/ExecutionProcessingServiceTests.cs
@@ -4,13 +4,16 @@
 // See License.txt in the project root for license information.
 // ---------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using Moq;
 using Standardly.Core.Brokers.Loggings;
 using Standardly.Core.Models.Foundations.Executions;
 using Standardly.Core.Services.Foundations.Executions;
 using Standardly.Core.Services.Processings.Executions;
 using Tynamix.ObjectFiller;
+using Xeptions;
 
 namespace Standardly.Core.Tests.Unit.Services.Processings.Executions
 {
@@ -29,6 +32,9 @@ namespace Standardly.Core.Tests.Unit.Services.Processings.Executions
                 executionService: this.executionServiceMock.Object,
                 loggingBroker: this.loggingBrokerMock.Object);
         }
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
 
         private static string GetRandomString() =>
             new MnemonicString().GetValue();
