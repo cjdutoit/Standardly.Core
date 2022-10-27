46f7b87923656efff714a89b471e93a12bdcc879
Christo du Toit <christo@dutoit.co.uk>
2022-10-27 15:40:17 +0100

    ShoudThrowServiceExceptionOnRunIfServiceErrorOccurs -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/Executions/ExecutionServiceTests.Exceptions.Run.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Foundations.Executions;
+using Standardly.Core.Models.Foundations.Executions.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.Executions
+{
+    public partial class ExecutionServiceTests
+    {
+        [Fact]
+        public void ShoudThrowServiceExceptionOnRunIfServiceErrorOccurs()
+        {
+            // given
+            string somePath = GetRandomString();
+            List<Execution> someExecutions = GetRandomExecutions();
+
+            var serviceException = new Exception();
+
+            var failedExecutionServiceException =
+                new FailedExecutionServiceException(serviceException);
+
+            var expectedExecutionServiceException =
+                new ExecutionServiceException(failedExecutionServiceException);
+
+            this.executionBrokerMock.Setup(broker =>
+                broker.Run(someExecutions, somePath))
+                    .Throws(serviceException);
+
+            // when
+            Action writeToFileAction = () =>
+                this.executionService.Run(someExecutions, somePath);
+
+            ExecutionServiceException actualException = Assert
+                .Throws<ExecutionServiceException>(writeToFileAction);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedExecutionServiceException);
+
+            this.executionBrokerMock.Verify(broker =>
+                broker.Run(someExecutions, somePath),
+                    Times.Once);
+
+            this.executionBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
