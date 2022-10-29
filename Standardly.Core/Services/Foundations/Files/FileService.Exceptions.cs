17a144e9d62b8eca2dc129d7eb2850a77f504a0a
Christo du Toit <christo@dutoit.co.uk>
2022-10-29 16:48:51 +0100

    ShouldThrowDependencyValidationExceptionOnReadFromFileIfDependencyValidationErrorOccursAndLogItAsync -> PASS

--- Standardly.Core/Services/Foundations/Files/FileService.Exceptions.cs
+++ Standardly.Core/Services/Foundations/Files/FileService.Exceptions.cs
@@ -97,6 +97,27 @@ namespace Standardly.Core.Services.Foundations.Files
             {
                 throw CreateAndLogValidationException(invalidArgumentFileException);
             }
+            catch (ArgumentNullException argumentNullException)
+            {
+                var invalidFileDependencyException =
+                    new InvalidFileServiceDependencyException(argumentNullException);
+
+                throw CreateAndLogDependencyValidationException(invalidFileDependencyException);
+            }
+            catch (ArgumentOutOfRangeException argumentOutOfRangeException)
+            {
+                var invalidFileDependencyException =
+                    new InvalidFileServiceDependencyException(argumentOutOfRangeException);
+
+                throw CreateAndLogDependencyValidationException(invalidFileDependencyException);
+            }
+            catch (ArgumentException argumentException)
+            {
+                var invalidFileDependencyException =
+                    new InvalidFileServiceDependencyException(argumentException);
+
+                throw CreateAndLogDependencyValidationException(invalidFileDependencyException);
+            }
         }
 
         private async ValueTask TryCatch(ReturningNothingFunction returningNothingFunction)
