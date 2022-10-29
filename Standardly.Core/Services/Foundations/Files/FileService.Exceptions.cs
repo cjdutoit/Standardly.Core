4c7310e464ff0b34fcb856c01e0602d164e1890b
Christo du Toit <christo@dutoit.co.uk>
2022-10-29 22:10:42 +0100

    ShouldThrowDependencyValidationExceptionOnRetrieveListOfFilesIfDependencyValidationErrorOccursAndLogItAsync -> PASS

--- Standardly.Core/Services/Foundations/Files/FileService.Exceptions.cs
+++ Standardly.Core/Services/Foundations/Files/FileService.Exceptions.cs
@@ -167,6 +167,62 @@ namespace Standardly.Core.Services.Foundations.Files
             {
                 throw CreateAndLogValidationException(invalidArgumentFileException);
             }
+            catch (ArgumentNullException argumentNullException)
+            {
+                var invalidFileDependencyException =
+                    new InvalidFileServiceDependencyException(argumentNullException);
+
+                throw CreateAndLogDependencyValidationException(invalidFileDependencyException);
+            }
+            catch (ArgumentOutOfRangeException argumentOutOfRangeException)
+            {
+                var invalidFileDependencyException =
+                    new InvalidFileServiceDependencyException(argumentOutOfRangeException);
+
+                throw CreateAndLogDependencyValidationException(invalidFileDependencyException);
+            }
+            catch (ArgumentException argumentException)
+            {
+                var invalidFileDependencyException =
+                    new InvalidFileServiceDependencyException(argumentException);
+
+                throw CreateAndLogDependencyValidationException(invalidFileDependencyException);
+            }
+            catch (SerializationException serializationException)
+            {
+                var failedFileDependencyException =
+                    new FailedFileDependencyException(serializationException);
+
+                throw CreateAndLogDependencyException(failedFileDependencyException);
+            }
+            catch (OutOfMemoryException outOfMemoryException)
+            {
+                var failedFileDependencyException =
+                    new FailedFileDependencyException(outOfMemoryException);
+
+                throw CreateAndLogCriticalDependencyException(failedFileDependencyException);
+            }
+            catch (IOException ioException)
+            {
+                var failedFileDependencyException =
+                    new FailedFileDependencyException(ioException);
+
+                throw CreateAndLogDependencyException(failedFileDependencyException);
+            }
+            catch (UnauthorizedAccessException unauthorizedAccessException)
+            {
+                var failedFileDependencyException =
+                    new FailedFileDependencyException(unauthorizedAccessException);
+
+                throw CreateAndLogCriticalDependencyException(failedFileDependencyException);
+            }
+            catch (Exception exception)
+            {
+                var failedFileServiceException =
+                    new FailedFileServiceException(exception);
+
+                throw CreateAndLogServiceException(failedFileServiceException);
+            }
         }
 
         private async ValueTask TryCatch(ReturningNothingFunction returningNothingFunction)
