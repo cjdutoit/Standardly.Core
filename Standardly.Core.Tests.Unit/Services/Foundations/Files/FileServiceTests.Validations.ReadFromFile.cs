3f1bb2919c7e2de7967172e3f6e0b69309f59d3a
Christo du Toit <christo@dutoit.co.uk>
2022-10-29 14:47:51 +0100

    ShouldThrowValidationExceptionOnReadFromFileIfPathIsInvalidAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/Files/FileServiceTests.Validations.ReadFromFile.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Foundations.Files.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.Files
+{
+    public partial class FileServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnReadFromFileIfPathIsInvalidAsync(string invalidPath)
+        {
+            // given
+            var invalidArgumentFileException =
+                new InvalidArgumentFileException();
+
+            invalidArgumentFileException.AddData(
+                key: "path",
+                values: "Text is required");
+
+            var expectedFileValidationException =
+                new FileValidationException(invalidArgumentFileException);
+
+            // when
+            ValueTask<string> checkIfFileExistsTask =
+                this.fileService.ReadFromFileAsync(invalidPath);
+
+            FileValidationException actualException =
+                await Assert.ThrowsAsync<FileValidationException>(checkIfFileExistsTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFileValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFileValidationException))),
+                        Times.Once);
+
+            this.fileBrokerMock.Verify(broker =>
+                broker.ReadFile(invalidPath),
+                    Times.Never);
+
+            this.fileBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
