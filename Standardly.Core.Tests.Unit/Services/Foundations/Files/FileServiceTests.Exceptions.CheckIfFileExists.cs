7bb6b9e3f3c93249bd44d644f20f48573c93b7f2
Christo du Toit <christo@dutoit.co.uk>
2022-10-29 11:33:45 +0100

    ShouldThrowDependencyExceptionOnCheckIfFileExistsIfDependencyErrorOccursAndLogIt -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/Files/FileServiceTests.Exceptions.CheckIfFileExists.cs
@@ -0,0 +1,106 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Foundations.Files.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.Files
+{
+    public partial class FileServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(FileServiceDependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnCheckIfFileExistsIfDependencyValidationErrorOccursAndLogIt(
+            Exception dependencyValidationException)
+        {
+            // given
+            string somePath = GetRandomString();
+
+            var invalidFileServiceDependencyException =
+                new InvalidFileServiceDependencyException(
+                    dependencyValidationException);
+
+            var expectedFileDependencyValidationException =
+                new FileDependencyValidationException(invalidFileServiceDependencyException);
+
+            this.fileBrokerMock.Setup(broker =>
+                broker.CheckIfFileExists(somePath))
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask<bool> writeToFileTask =
+                this.fileService.CheckIfFileExistsAsync(somePath);
+
+            FileDependencyValidationException actualException =
+                await Assert.ThrowsAsync<FileDependencyValidationException>(writeToFileTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFileDependencyValidationException);
+
+            this.fileBrokerMock.Verify(broker =>
+                broker.CheckIfFileExists(somePath),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFileDependencyValidationException))),
+                        Times.Once);
+
+            this.fileBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(FileServiceDependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnCheckIfFileExistsIfDependencyErrorOccursAndLogIt(
+            Exception dependencyException)
+        {
+            // given
+            string somePath = GetRandomString();
+
+            var invalidFileServiceDependencyException =
+                new InvalidFileServiceDependencyException(
+                    dependencyException);
+
+            var failedFileDependencyException =
+                new FailedFileDependencyException(
+                    invalidFileServiceDependencyException);
+
+            var expectedFileDependencyException =
+                new FileDependencyException(failedFileDependencyException);
+
+            this.fileBrokerMock.Setup(broker =>
+                broker.CheckIfFileExists(somePath))
+                    .Throws(dependencyException);
+
+            // when
+            ValueTask<bool> writeToFileAction =
+                this.fileService.CheckIfFileExistsAsync(somePath);
+
+            FileDependencyException actualException =
+                await Assert.ThrowsAsync<FileDependencyException>(writeToFileAction.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFileDependencyException);
+
+            this.fileBrokerMock.Verify(broker =>
+                broker.CheckIfFileExists(somePath),
+                    Times.Exactly(this.retryConfig.MaxRetryAttempts));
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFileDependencyException))),
+                        Times.Once);
+
+            this.fileBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
