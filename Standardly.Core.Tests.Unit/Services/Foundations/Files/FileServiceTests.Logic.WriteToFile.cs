2a0ccb15a34a862226e11b8679b77391b3a5c2d6
Christo du Toit <christo@dutoit.co.uk>
2022-10-29 12:30:11 +0100

    ShouldWriteToFile -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/Files/FileServiceTests.Logic.WriteToFile.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.Files
+{
+    public partial class FileServiceTests
+    {
+        [Fact]
+        public async Task ShouldWriteToFile()
+        {
+            // given
+            string randomFilePath = GetRandomString();
+            string inputFilePath = randomFilePath;
+            string randomContent = GetRandomString();
+            string inputContent = randomContent;
+
+            // when
+            await this.fileService.WriteToFileAsync(inputFilePath, inputContent);
+
+            // then
+            this.fileBrokerMock.Verify(broker =>
+                broker.WriteToFile(inputFilePath, inputContent),
+                    Times.Once);
+
+            this.fileBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
