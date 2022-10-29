74d6e8151f6d328c080a18ca7039c7c71dea02e5
Christo du Toit <christo@dutoit.co.uk>
2022-10-29 16:48:41 +0100

    ShouldThrowDependencyValidationExceptionOnReadFromFileIfDependencyValidationErrorOccursAndLogItAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/Files/FileServiceTests.Exceptions.ReadFromFile.cs
@@ -0,0 +1,60 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Foundations.Files.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.Files
+{
+    public partial class FileServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(FileServiceDependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnReadFromFileIfDependencyValidationErrorOccursAndLogItAsync(
+            Exception dependencyValidationException)
+        {
+            // given
+            string somePath = GetRandomString();
+
+            var invalidFileServiceDependencyException =
+                new InvalidFileServiceDependencyException(
+                    dependencyValidationException);
+
+            var expectedFileDependencyValidationException =
+                new FileDependencyValidationException(invalidFileServiceDependencyException);
+
+            this.fileBrokerMock.Setup(broker =>
+                broker.ReadFile(somePath))
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask<string> writeToFileTask =
+                this.fileService.ReadFromFileAsync(somePath);
+
+            FileDependencyValidationException actualException =
+                await Assert.ThrowsAsync<FileDependencyValidationException>(writeToFileTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFileDependencyValidationException);
+
+            this.fileBrokerMock.Verify(broker =>
+                broker.ReadFile(somePath),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFileDependencyValidationException))),
+                        Times.Once);
+
+            this.fileBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
