912f66e078440080014333d352c49deabae13627
Christo du Toit <christo@dutoit.co.uk>
2022-10-30 12:36:17 +0000

    ShouldThrowDependencyValidationExceptionOnDeleteDirectoryIfDependencyValidationErrorOccursAndLogItAsync -> PASS

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/Files/FileServiceTests.Exceptions.DeleteDirectory.cs
@@ -0,0 +1,61 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Foundations.Files.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.Files
+{
+    public partial class FileServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(FileServiceDependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnDeleteDirectoryIfDependencyValidationErrorOccursAndLogItAsync(
+            Exception dependencyValidationException)
+        {
+            // given
+            string somePath = GetRandomString();
+            bool recursive = true;
+
+            var invalidFileServiceDependencyException =
+                new InvalidFileServiceDependencyException(
+                    dependencyValidationException);
+
+            var expectedFileDependencyValidationException =
+                new FileDependencyValidationException(invalidFileServiceDependencyException);
+
+            this.fileBrokerMock.Setup(broker =>
+                broker.DeleteDirectory(somePath, recursive))
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask writeToFileTask =
+                this.fileService.DeleteDirectoryAsync(somePath, recursive);
+
+            FileDependencyValidationException actualException =
+                await Assert.ThrowsAsync<FileDependencyValidationException>(writeToFileTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFileDependencyValidationException);
+
+            this.fileBrokerMock.Verify(broker =>
+                broker.DeleteDirectory(somePath, recursive),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFileDependencyValidationException))),
+                        Times.Once);
+
+            this.fileBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
