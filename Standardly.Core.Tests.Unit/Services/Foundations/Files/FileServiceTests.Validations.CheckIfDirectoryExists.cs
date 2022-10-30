b2881520ca340c64c534ac9948d3a180d1541feb
Christo du Toit <christo@dutoit.co.uk>
2022-10-30 10:26:40 +0000

    ShouldThrowValidationExceptionOnCheckIfDirectoryExistsIfPathIsInvalidAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/Files/FileServiceTests.Validations.CheckIfDirectoryExists.cs
@@ -0,0 +1,58 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Foundations.Files.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.Files
+{
+    public partial class FileServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnCheckIfDirectoryExistsIfPathIsInvalidAsync(string invalidPath)
+        {
+            // given
+            var invalidArgumentFileException =
+                new InvalidArgumentFileException();
+
+            invalidArgumentFileException.AddData(
+                key: "path",
+                values: "Text is required");
+
+            var expectedFileValidationException =
+                new FileValidationException(invalidArgumentFileException);
+
+            // when
+            ValueTask<bool> checkIfFileExistsTask =
+                this.fileService.CheckIfDirectoryExistsAsync(invalidPath);
+
+            FileValidationException actualException =
+                await Assert.ThrowsAsync<FileValidationException>(checkIfFileExistsTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFileValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFileValidationException))),
+                        Times.Once);
+
+            this.fileBrokerMock.Verify(broker =>
+                broker.CheckIfDirectoryExists(
+                    It.IsAny<string>()),
+                        Times.Never);
+
+            this.fileBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
