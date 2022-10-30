ebe9841b4f8d19aa7f30a2a8ea375d6cbe4d46a0
Christo du Toit <christo@dutoit.co.uk>
2022-10-30 13:32:38 +0000

    ShouldCheckIfFileExists -> FAIL

---
+++ Standardly.Core/Services/Processings/Files/FileProcessingService.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Standardly.Core.Brokers.Loggings;
+using Standardly.Core.Services.Foundations.Files;
+
+namespace Standardly.Core.Services.Processings.Files
+{
+    public partial class FileProcessingService : IFileProcessingService
+    {
+        private readonly IFileService fileService;
+        private readonly ILoggingBroker loggingBroker;
+
+        public FileProcessingService(IFileService fileService, ILoggingBroker loggingBroker)
+        {
+            this.fileService = fileService;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<bool> CheckIfFileExistsAsync(string path) =>
+            throw new System.NotImplementedException();
+    }
+}
