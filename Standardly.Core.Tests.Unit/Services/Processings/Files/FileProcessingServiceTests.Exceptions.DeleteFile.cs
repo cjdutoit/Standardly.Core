d5043b4ca1ed5b1d193330ed6aa122d8f70bfacc
Christo du Toit <christo@dutoit.co.uk>
2022-10-31 17:10:25 +0000

    ShouldThrowServiceExceptionOnDeleteFileAsyncIfServiceErrorOccursAndLogItAsync -> PASS

--- Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Exceptions.DeleteFile.cs
+++ Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Exceptions.DeleteFile.cs
@@ -4,6 +4,7 @@
 // See License.txt in the project root for license information.
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using Moq;
 using Standardly.Core.Models.Processings.Files.Exceptions;
@@ -84,6 +85,48 @@ namespace Standardly.Core.Tests.Unit.Services.Processings.Files
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
                     expectedFileProcessingDependencyException))),
+                        Times.Once);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnDeleteFileAsyncIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputPath = randomPath;
+            string inputContent = randomPath;
+
+            var serviceException = new Exception();
+
+            var failedFileProcessingServiceException =
+                new FailedFileProcessingServiceException(serviceException);
+
+            var expectedFileProcessingServiveException =
+                new FileProcessingServiceException(
+                    failedFileProcessingServiceException);
+
+            this.fileServiceMock.Setup(service =>
+                service.DeleteFileAsync(inputPath))
+                    .Throws(serviceException);
+
+            // when
+            ValueTask runTask =
+                this.fileProcessingService.DeleteFileAsync(inputPath);
+
+            // then
+            FileProcessingServiceException actualException =
+                await Assert.ThrowsAsync<FileProcessingServiceException>(runTask.AsTask);
+
+            this.fileServiceMock.Verify(service =>
+                service.DeleteFileAsync(inputPath),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFileProcessingServiveException))),
                         Times.Once);
 
             this.fileServiceMock.VerifyNoOtherCalls();
