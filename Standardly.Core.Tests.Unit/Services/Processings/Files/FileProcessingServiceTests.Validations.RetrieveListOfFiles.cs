b5aa787f18f5b517b70f080db00cd5bdb18c85da
Christo du Toit <christo@dutoit.co.uk>
2022-10-31 19:41:24 +0000

    ShouldThrowValidationExceptionOnRetrieveListOfFilesIfInputsIsInvalidAndLogItAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Validations.RetrieveListOfFiles.cs
@@ -0,0 +1,67 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Processings.Files.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.Files
+{
+    public partial class FileProcessingServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnRetrieveListOfFilesIfInputsIsInvalidAndLogItAsync(
+            string invalidInput)
+        {
+            // given
+            string invalidPath = invalidInput;
+            string invalidSearchPattern = invalidInput;
+
+            var invalidFilesProcessingException =
+                new InvalidFileProcessingException();
+
+            invalidFilesProcessingException.AddData(
+                key: "path",
+                values: "Text is required");
+
+            invalidFilesProcessingException.AddData(
+                key: "searchPattern",
+                values: "Text is required");
+
+            var expectedFilesProcessingValidationException =
+                new FileProcessingValidationException(invalidFilesProcessingException);
+
+            // when
+            ValueTask<List<string>> runTask =
+                this.fileProcessingService
+                    .RetrieveListOfFilesAsync(path: invalidPath, searchPattern: invalidSearchPattern);
+
+            FileProcessingValidationException actualException =
+                await Assert.ThrowsAsync<FileProcessingValidationException>(runTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFilesProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFilesProcessingValidationException))),
+                        Times.Once);
+
+            this.fileServiceMock.Verify(service =>
+                service.RetrieveListOfFilesAsync(invalidPath, invalidSearchPattern),
+                    Times.Never);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
