3b6d77cb2685296108ee5bd87e16d11fff81cfcd
Christo du Toit <christo@dutoit.co.uk>
2022-10-31 17:10:09 +0000

    ShouldThrowDependencyOnDeleteFileAsyncIfDependencyErrorOccursAndLogItAsync -> PASS

--- Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Exceptions.DeleteFile.cs
+++ Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Exceptions.DeleteFile.cs
@@ -51,5 +51,43 @@ namespace Standardly.Core.Tests.Unit.Services.Processings.Files
             this.fileServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyOnDeleteFileAsyncIfDependencyErrorOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputPath = randomPath;
+
+            var expectedFileProcessingDependencyException =
+                new FileProcessingDependencyException(
+                    dependencyException.InnerException as Xeption);
+
+            this.fileServiceMock.Setup(service =>
+                service.DeleteFileAsync(inputPath))
+                    .Throws(dependencyException);
+
+            // when
+            ValueTask runTask =
+                this.fileProcessingService.DeleteFileAsync(inputPath);
+
+            // then
+            FileProcessingDependencyException actualException =
+                await Assert.ThrowsAsync<FileProcessingDependencyException>(runTask.AsTask);
+
+            this.fileServiceMock.Verify(service =>
+                service.DeleteFileAsync(inputPath),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFileProcessingDependencyException))),
+                        Times.Once);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
