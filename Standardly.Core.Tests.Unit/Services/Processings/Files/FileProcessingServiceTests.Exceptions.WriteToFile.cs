2761cbde9240367252b4e98184b181b4bac456f9
Christo du Toit <christo@dutoit.co.uk>
2022-10-31 13:13:44 +0000

    ShouldThrowDependencyValidationOnWriteToFileAsyncIfDependencyValidationErrorOccursAndLogItAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Exceptions.WriteToFile.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Moq;
+using Standardly.Core.Models.Processings.Files.Exceptions;
+using Xeptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.Files
+{
+    public partial class FileProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationOnWriteToFileAsyncIfDependencyValidationErrorOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputPath = randomPath;
+            string inputContent = randomPath;
+
+            var expectedFileProcessingDependencyValidationException =
+                new FileProcessingDependencyValidationException(
+                    dependencyValidationException.InnerException as Xeption);
+
+            this.fileServiceMock.Setup(service =>
+                service.WriteToFileAsync(inputPath, inputContent))
+                    .Throws(dependencyValidationException);
+
+            // when
+            ValueTask runTask =
+                this.fileProcessingService.WriteToFileAsync(inputPath, inputContent);
+
+            // then
+            FileProcessingDependencyValidationException actualException =
+                await Assert.ThrowsAsync<FileProcessingDependencyValidationException>(runTask.AsTask);
+
+            this.fileServiceMock.Verify(service =>
+                service.WriteToFileAsync(inputPath, inputContent),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFileProcessingDependencyValidationException))),
+                        Times.Once);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
