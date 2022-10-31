354718008cec1118e5375a2998e581dc111e0a21
Christo du Toit <christo@dutoit.co.uk>
2022-10-31 23:45:37 +0000

    ShouldThrowValidationExceptionOnCreateDirectoryIfInputsIsInvalidAndLogItAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Validations.CreateDirectory.cs
@@ -0,0 +1,60 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Processings.Files.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.Files
+{
+    public partial class FileProcessingServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnCreateDirectoryIfInputsIsInvalidAndLogItAsync(
+            string invalidInput)
+        {
+            // given
+            string invalidPath = invalidInput;
+
+            var invalidFilesProcessingException =
+                new InvalidFileProcessingException();
+
+            invalidFilesProcessingException.AddData(
+                key: "path",
+                values: "Text is required");
+
+            var expectedFilesProcessingValidationException =
+                new FileProcessingValidationException(invalidFilesProcessingException);
+
+            // when
+            ValueTask runTask =
+                this.fileProcessingService.CreateDirectoryAsync(path: invalidPath);
+
+            FileProcessingValidationException actualException =
+                await Assert.ThrowsAsync<FileProcessingValidationException>(runTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFilesProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFilesProcessingValidationException))),
+                        Times.Once);
+
+            this.fileServiceMock.Verify(broker =>
+                broker.CreateDirectoryAsync(invalidPath),
+                    Times.Never);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
