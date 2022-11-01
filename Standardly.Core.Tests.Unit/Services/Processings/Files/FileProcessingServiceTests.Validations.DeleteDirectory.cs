45f70e4ef9faaefb1cc0e4e157f5955cf33b12e4
Christo du Toit <christo@dutoit.co.uk>
2022-11-01 00:09:50 +0000

    ShouldThrowValidationExceptionOnDeleteDirectoryIfInputsIsInvalidAndLogItAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Processings/Files/FileProcessingServiceTests.Validations.DeleteDirectory.cs
@@ -0,0 +1,61 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Processings.Files.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.Files
+{
+    public partial class FileProcessingServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnDeleteDirectoryIfInputsIsInvalidAndLogItAsync(
+            string invalidInput)
+        {
+            // given
+            string invalidPath = invalidInput;
+            bool recursive = false;
+
+            var invalidFilesProcessingException =
+                new InvalidFileProcessingException();
+
+            invalidFilesProcessingException.AddData(
+                key: "path",
+                values: "Text is required");
+
+            var expectedFilesProcessingValidationException =
+                new FileProcessingValidationException(invalidFilesProcessingException);
+
+            // when
+            ValueTask deleteDirectoryTask =
+                this.fileProcessingService.DeleteDirectoryAsync(path: invalidPath, recursive);
+
+            FileProcessingValidationException actualException =
+                await Assert.ThrowsAsync<FileProcessingValidationException>(deleteDirectoryTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFilesProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedFilesProcessingValidationException))),
+                        Times.Once);
+
+            this.fileServiceMock.Verify(service =>
+                service.DeleteDirectoryAsync(invalidPath, recursive),
+                    Times.Never);
+
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
