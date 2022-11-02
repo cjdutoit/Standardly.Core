51161c626ebe1909dfb98f46b914a0dca67bd008
Christo du Toit <christo@dutoit.co.uk>
2022-11-02 16:40:05 +0000

    ShouldThrowValidationExceptionOnTransformStringIfContentIsNullOrEmpty -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/Templates/TemplateServiceTests.Validations.TransformString.cs
@@ -0,0 +1,49 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Standardly.Core.Models.Foundations.Templates.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.Templates
+{
+    public partial class TemplateServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnTransformStringIfContentIsNullOrEmpty(string invalidString)
+        {
+            // given
+            string content = invalidString;
+            Dictionary<string, string> randomReplacementDictionary = CreateReplacementDictionary();
+            Dictionary<string, string> inputReplacementDictionary = randomReplacementDictionary;
+
+            var invalidTemplateException =
+                            new InvalidTemplateException();
+
+            invalidTemplateException.AddData(
+                key: "content",
+                values: "Text is required");
+
+            var expectedTemplateValidationException =
+                new TemplateValidationException(invalidTemplateException);
+
+            // when
+            ValueTask<string> transformStringTask =
+                this.templateService.TransformString(content, inputReplacementDictionary);
+
+            var actualException =
+                await Assert.ThrowsAsync<TemplateValidationException>(transformStringTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedTemplateValidationException);
+        }
+    }
+}
