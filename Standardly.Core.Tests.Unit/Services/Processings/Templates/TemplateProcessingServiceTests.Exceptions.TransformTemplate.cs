3f3b69bd8fdbfd2116777e06e8fa521490437a24
Christo du Toit <christo@dutoit.co.uk>
2022-11-04 18:34:20 +0000

    ShouldThrowServiceExceptionOnTransformTemplateIfServiceErrorOccursAndLogItAsync -> PASS

--- Standardly.Core.Tests.Unit/Services/Processings/Templates/TemplateProcessingServiceTests.Exceptions.TransformTemplate.cs
+++ Standardly.Core.Tests.Unit/Services/Processings/Templates/TemplateProcessingServiceTests.Exceptions.TransformTemplate.cs
@@ -4,6 +4,7 @@
 // See License.txt in the project root for license information.
 // ---------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Moq;
@@ -102,6 +103,59 @@ namespace Standardly.Core.Tests.Unit.Services.Processings.Templates
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
                     expectedTemplateProcessingDependencyException))),
+                        Times.Once);
+
+            this.templateServiceMock.Verify(service =>
+                service.ValidateTransformationAsync(It.IsAny<string>(), inputTagCharacter),
+                    Times.Never());
+
+            this.templateServiceMock.Verify(service =>
+                service.ConvertStringToTemplateAsync(It.IsAny<string>()),
+                    Times.Never());
+
+            this.templateServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnTransformTemplateIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            Template randomInputTemplate = CreateRandomTemplate();
+            Template inputTemplate = randomInputTemplate;
+            Dictionary<string, string> randomReplacementDictionary = CreateReplacementDictionary();
+            Dictionary<string, string> inputReplacementDictionary = randomReplacementDictionary;
+            char inputTagCharacter = '$';
+
+            var serviceException = new Exception();
+
+            var failedTemplateProcessingServiceException =
+                new FailedTemplateProcessingServiceException(serviceException);
+
+            var expectedTemplateProcessingServiveException =
+                new TemplateProcessingServiceException(
+                    failedTemplateProcessingServiceException);
+
+            this.templateServiceMock.Setup(service =>
+                service.TransformStringAsync(inputTemplate.RawTemplate, inputReplacementDictionary))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<Template> transformTemplateTask =
+                this.templateProcessingService
+                    .TransformTemplateAsync(inputTemplate, inputReplacementDictionary, inputTagCharacter);
+
+            // then
+            TemplateProcessingServiceException actualException =
+                await Assert.ThrowsAsync<TemplateProcessingServiceException>(transformTemplateTask.AsTask);
+
+            this.templateServiceMock.Verify(service =>
+                service.TransformStringAsync(inputTemplate.RawTemplate, inputReplacementDictionary),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTemplateProcessingServiveException))),
                         Times.Once);
 
             this.templateServiceMock.Verify(service =>
