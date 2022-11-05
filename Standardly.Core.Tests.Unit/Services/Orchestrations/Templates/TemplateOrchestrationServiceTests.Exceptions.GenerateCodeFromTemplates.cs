11f6474911a0403c8b303938aecf0e01deed00dd
Christo du Toit <christo@dutoit.co.uk>
2022-11-05 22:54:16 +0000

    ShoudThrowServiceExceptionOnGenerateCodeFromTemplateAndLogItAsync -> FAIL

--- Standardly.Core.Tests.Unit/Services/Orchestrations/Templates/TemplateOrchestrationServiceTests.Exceptions.GenerateCodeFromTemplates.cs
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/Templates/TemplateOrchestrationServiceTests.Exceptions.GenerateCodeFromTemplates.cs
@@ -99,5 +99,47 @@ namespace Standardly.Core.Tests.Unit.Services.Orchestrations.Templates
             this.executionProcessingServiceMock.VerifyNoOtherCalls();
             this.templateProcessingServiceMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShoudThrowServiceExceptionOnGenerateCodeFromTemplateAndLogItAsync()
+        {
+            // given
+            int randomNumber = 1; //GetRandomNumber();
+            List<Template> randomTemplates = GetRandomTemplateList(randomNumber, true);
+            List<Template> inputTemplates = randomTemplates;
+            Dictionary<string, string> randomReplacementDictionary = CreateReplacementDictionary();
+            Dictionary<string, string> inputDictionary = randomReplacementDictionary;
+            var serviceException = new Exception();
+
+            var failedTemplateOrchestrationServiceException =
+                new FailedTemplateOrchestrationServiceException(serviceException);
+
+            var expectedTemplateOrchestrationServiceException =
+                new TemplateOrchestrationServiceException(failedTemplateOrchestrationServiceException);
+
+            this.templateProcessingServiceMock.Setup(service =>
+                service.TransformTemplateAsync(
+                    It.IsAny<Template>(),
+                    inputDictionary))
+                        .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<List<Template>> findAllTemplatesTask =
+                this.templateOrchestrationService.FindAllTemplatesAsync();
+
+            TemplateOrchestrationServiceException actualException =
+                await Assert.ThrowsAsync<TemplateOrchestrationServiceException>(findAllTemplatesTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedTemplateOrchestrationServiceException);
+
+            this.templateProcessingServiceMock.Verify(broker =>
+                broker.TransformTemplateAsync(It.IsAny<Template>(), inputDictionary),
+                    Times.Once);
+
+            this.fileProcessingServiceMock.VerifyNoOtherCalls();
+            this.executionProcessingServiceMock.VerifyNoOtherCalls();
+            this.templateProcessingServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
