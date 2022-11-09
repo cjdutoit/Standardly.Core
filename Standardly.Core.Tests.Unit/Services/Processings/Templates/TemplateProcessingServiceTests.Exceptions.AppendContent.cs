17d210d55d8df53c79d5bd82999a266f4645f0ba
Christo du Toit <christo@dutoit.co.uk>
2022-11-09 18:28:54 +0000

    ShouldThrowDependencyValidationOnAppendContentIfDependencyValidationErrorOccursAndLogItAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Processings/Templates/TemplateProcessingServiceTests.Exceptions.AppendContent.cs
@@ -0,0 +1,201 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using Standardly.Core.Models.Processings.Templates.Exceptions;
+using Xeptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.Templates
+{
+    public partial class TemplateProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationOnAppendContentIfDependencyValidationErrorOccursAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            string sourceContent = GetRandomString();
+            string regexToMatch = GetRandomString();
+            string appendContent = GetRandomString();
+            bool appendToBeginning = false;
+            bool onlyAppendIfNotPresent = true;
+
+            var expectedTemplateProcessingDependencyValidationException =
+                new TemplateProcessingDependencyValidationException(
+                    dependencyValidationException.InnerException as Xeption);
+
+            this.templateServiceMock.Setup(service =>
+                service.AppendContentAsync(
+                    sourceContent,
+                    regexToMatch,
+                    appendContent,
+                    appendToBeginning,
+                    onlyAppendIfNotPresent))
+                        .ThrowsAsync(dependencyValidationException);
+
+            // when
+            ValueTask<string> appendContentTask =
+                this.templateProcessingService
+                    .AppendContentAsync(
+                        sourceContent,
+                        regexToMatch,
+                        appendContent,
+                        appendToBeginning,
+                        onlyAppendIfNotPresent);
+
+            // then
+            TemplateProcessingDependencyValidationException actualException =
+                await Assert
+                    .ThrowsAsync<TemplateProcessingDependencyValidationException>(appendContentTask.AsTask);
+
+            this.templateServiceMock.Verify(service =>
+                service.AppendContentAsync(
+                    sourceContent,
+                    regexToMatch,
+                    appendContent,
+                    appendToBeginning,
+                    onlyAppendIfNotPresent),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTemplateProcessingDependencyValidationException))),
+                        Times.Once);
+
+            this.templateServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyOnAppendContentIfDependencyErrorOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            string sourceContent = GetRandomString();
+            string regexToMatch = GetRandomString();
+            string appendContent = GetRandomString();
+            bool appendToBeginning = false;
+            bool onlyAppendIfNotPresent = true;
+
+            var expectedTemplateProcessingDependencyException =
+                new TemplateProcessingDependencyException(
+                    dependencyException.InnerException as Xeption);
+
+            this.templateServiceMock.Setup(service =>
+                service.AppendContentAsync(
+                    sourceContent,
+                    regexToMatch,
+                    appendContent,
+                    appendToBeginning,
+                    onlyAppendIfNotPresent))
+                        .ThrowsAsync(dependencyException);
+
+            // when
+            ValueTask<string> appendContentTask =
+                this.templateProcessingService
+                    .AppendContentAsync(
+                        sourceContent,
+                        regexToMatch,
+                        appendContent,
+                        appendToBeginning,
+                        onlyAppendIfNotPresent);
+
+            // then
+            TemplateProcessingDependencyException actualException =
+                await Assert.ThrowsAsync<TemplateProcessingDependencyException>(appendContentTask.AsTask);
+
+            this.templateServiceMock.Verify(service =>
+                 service.AppendContentAsync(
+                     sourceContent,
+                     regexToMatch,
+                     appendContent,
+                     appendToBeginning,
+                     onlyAppendIfNotPresent),
+                         Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTemplateProcessingDependencyException))),
+                        Times.Once);
+
+            this.templateServiceMock.Verify(service =>
+                service.ValidateTransformationAsync(It.IsAny<string>()),
+                    Times.Never());
+
+            this.templateServiceMock.Verify(service =>
+                service.ConvertStringToTemplateAsync(It.IsAny<string>()),
+                    Times.Never());
+
+            this.templateServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnAppendContentIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            string sourceContent = GetRandomString();
+            string regexToMatch = GetRandomString();
+            string appendContent = GetRandomString();
+            bool appendToBeginning = false;
+            bool onlyAppendIfNotPresent = true;
+
+            var serviceException = new Exception();
+
+            var failedTemplateProcessingServiceException =
+                new FailedTemplateProcessingServiceException(serviceException);
+
+            var expectedTemplateProcessingServiveException =
+                new TemplateProcessingServiceException(
+                    failedTemplateProcessingServiceException);
+
+            this.templateServiceMock.Setup(service =>
+                service.AppendContentAsync(
+                    sourceContent,
+                    regexToMatch,
+                    appendContent,
+                    appendToBeginning,
+                    onlyAppendIfNotPresent))
+                        .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<string> appendContentTask =
+                this.templateProcessingService
+                    .AppendContentAsync(
+                        sourceContent,
+                        regexToMatch,
+                        appendContent,
+                        appendToBeginning,
+                        onlyAppendIfNotPresent);
+
+            // then
+            TemplateProcessingServiceException actualException =
+                await Assert.ThrowsAsync<TemplateProcessingServiceException>(appendContentTask.AsTask);
+
+            this.templateServiceMock.Verify(service =>
+                 service.AppendContentAsync(
+                     sourceContent,
+                     regexToMatch,
+                     appendContent,
+                     appendToBeginning,
+                     onlyAppendIfNotPresent),
+                         Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTemplateProcessingServiveException))),
+                        Times.Once);
+
+            this.templateServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
