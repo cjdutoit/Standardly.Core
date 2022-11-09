dff6c2ed4b4e1bce0a9c0f39f3caf9093cf56a1c
Christo du Toit <christo@dutoit.co.uk>
2022-11-09 18:41:27 +0000

    ShouldThrowDependencyOnAppendContentIfDependencyErrorOccursAndLogItAsync -> PASS

--- Standardly.Core.Tests.Unit/Services/Processings/Templates/TemplateProcessingServiceTests.Exceptions.AppendContent.cs
+++ Standardly.Core.Tests.Unit/Services/Processings/Templates/TemplateProcessingServiceTests.Exceptions.AppendContent.cs
@@ -70,5 +70,70 @@ namespace Standardly.Core.Tests.Unit.Services.Processings.Templates
             this.templateServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyOnAppendContentIfDependencyErrorOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            string sourceContent = GetRandomString();
+            string regexToMatch = GetRandomString();
+            string appendContent = GetRandomString();
+            bool appendToBeginning = false;
+            bool onlyAppendIfNotPresent = true;
+
+            var expectedTemplateProcessingDependencyException =
+                new TemplateProcessingDependencyException(
+                    dependencyException.InnerException as Xeption);
+
+            this.templateServiceMock.Setup(service =>
+                service.AppendContentAsync(
+                    sourceContent,
+                    regexToMatch,
+                    appendContent,
+                    appendToBeginning,
+                    onlyAppendIfNotPresent))
+                        .ThrowsAsync(dependencyException);
+
+            // when
+            ValueTask<string> appendContentTask =
+                this.templateProcessingService
+                    .AppendContentAsync(
+                        sourceContent,
+                        regexToMatch,
+                        appendContent,
+                        appendToBeginning,
+                        onlyAppendIfNotPresent);
+
+            // then
+            TemplateProcessingDependencyException actualException =
+                await Assert.ThrowsAsync<TemplateProcessingDependencyException>(appendContentTask.AsTask);
+
+            this.templateServiceMock.Verify(service =>
+                 service.AppendContentAsync(
+                     sourceContent,
+                     regexToMatch,
+                     appendContent,
+                     appendToBeginning,
+                     onlyAppendIfNotPresent),
+                         Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTemplateProcessingDependencyException))),
+                        Times.Once);
+
+            this.templateServiceMock.Verify(service =>
+                service.ValidateTransformationAsync(It.IsAny<string>()),
+                    Times.Never());
+
+            this.templateServiceMock.Verify(service =>
+                service.ConvertStringToTemplateAsync(It.IsAny<string>()),
+                    Times.Never());
+
+            this.templateServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
