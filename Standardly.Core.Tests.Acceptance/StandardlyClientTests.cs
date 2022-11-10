fac64d94268ae6a064e2c8ebe9e2451f97d8d815
Christo du Toit <christo@dutoit.co.uk>
2022-11-10 22:02:14 +0000

    ShouldFindListOfTemplatesWithCustomConfigAsync -> PASS

--- Standardly.Core.Tests.Acceptance/StandardlyClientTests.cs
+++ Standardly.Core.Tests.Acceptance/StandardlyClientTests.cs
@@ -5,7 +5,9 @@
 // ---------------------------------------------------------------
 
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Standardly.Core.Clients;
@@ -21,6 +23,25 @@ namespace Standardly.Core.Tests.Acceptance
         {
             //given
             var standardlyClient = new StandardlyClient();
+
+            //when
+            List<Template> templates = await standardlyClient.FindAllTemplatesAsync();
+
+            //then
+            templates.Count().Should().Be(3);
+            templates.Any(template => template.Name == "BROKERS: DateTime Broker").Should().BeTrue();
+            templates.Any(template => template.Name == "BROKERS: Logging Broker").Should().BeTrue();
+            templates.Any(template => template.Name == "BROKERS: Storage Broker").Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task ShouldFindListOfTemplatesWithCustomConfigAsync()
+        {
+            //given
+            string assembly = Assembly.GetExecutingAssembly().Location;
+            string templateFolderPath = Path.Combine(Path.GetDirectoryName(assembly), @"Templates");
+            string templateDefinitionFileName = "Template.json";
+            var standardlyClient = new StandardlyClient(templateFolderPath, templateDefinitionFileName);
 
             //when
             List<Template> templates = await standardlyClient.FindAllTemplatesAsync();
