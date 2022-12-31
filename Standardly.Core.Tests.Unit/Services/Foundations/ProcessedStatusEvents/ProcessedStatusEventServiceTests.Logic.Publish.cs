5ffda273897d690dfd6fa99c9855525dab30bc8a
Christo du Toit <christo@dutoit.co.uk>
2022-12-31 17:13:58 +0000

    ShouldPublishProcessedStatusAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/ProcessedStatusEvents/ProcessedStatusEventServiceTests.Logic.Publish.cs
@@ -0,0 +1,41 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Standardly.Core.Models.Events.ProcessedStatuses;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.ProcessedStatusEvents
+{
+    public partial class ProcessedStatusEventServiceTests
+    {
+        [Fact]
+        public async Task ShouldPublishProcessedStatusAsync()
+        {
+            // given
+            ProcessedStatus randomProcessedStatus = CreateRandomProcessedStatus();
+            ProcessedStatus inputProcessedStatus = randomProcessedStatus;
+            ProcessedStatus expectedProcessedStatus = inputProcessedStatus.DeepClone();
+
+            // when
+            ProcessedStatus actualProcessedStatus =
+                await this.processedStatusEventService
+                    .PublishProcessedStatusAsync(inputProcessedStatus);
+
+            // then
+            actualProcessedStatus.Should().BeEquivalentTo(expectedProcessedStatus);
+
+            this.eventBrokerMock.Verify(broker =>
+                broker.PublishProcessedEventAsync(inputProcessedStatus),
+                    Times.Once);
+
+            this.eventBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- Standardly.Core.Tests.Unit/Services/Foundations/ProcessedStatusEvents/ProcessedStatusEventServiceTests.cs
+++ Standardly.Core.Tests.Unit/Services/Foundations/ProcessedStatusEvents/ProcessedStatusEventServiceTests.cs
@@ -4,9 +4,12 @@
 // See License.txt in the project root for license information.
 // ---------------------------------------------------------------
 
+using System;
 using Moq;
 using Standardly.Core.Brokers.Events;
+using Standardly.Core.Models.Events.ProcessedStatuses;
 using Standardly.Core.Services.Foundations.ProcessedStatusEvents;
+using Tynamix.ObjectFiller;
 
 namespace Standardly.Core.Tests.Unit.Services.Foundations.ProcessedStatusEvents
 {
@@ -21,6 +24,32 @@ namespace Standardly.Core.Tests.Unit.Services.Foundations.ProcessedStatusEvents
 
             this.processedStatusEventService = new ProcessedStatusEventService(
                 eventBroker: this.eventBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static string GetRandomString() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static ProcessedStatus CreateRandomProcessedStatus(DateTimeOffset? dateTimeOffset = null) =>
+            CreateProcessedStatusFiller(dateTimeOffset ?? GetRandomDateTimeOffset()).Create();
+
+        private static Filler<ProcessedStatus> CreateProcessedStatusFiller(DateTimeOffset dateTimeOffset)
+        {
+            var filler = new Filler<ProcessedStatus>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnProperty(borough => borough.Message).Use(GetRandomString())
+                .OnProperty(borough => borough.Status).Use(GetRandomString())
+                .OnProperty(borough => borough.ProcessedItems).Use(GetRandomNumber())
+                .OnProperty(borough => borough.TotalItems).Use(GetRandomNumber());
+
+            return filler;
         }
     }
 }
