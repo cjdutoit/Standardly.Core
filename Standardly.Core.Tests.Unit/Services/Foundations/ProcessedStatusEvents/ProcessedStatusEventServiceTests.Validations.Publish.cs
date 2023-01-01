10c2a15b877eb682c2f8e4ba796972ac6f494a3b
Christo du Toit <christo@dutoit.co.uk>
2023-01-01 12:10:14 +0000

    ShouldThrowValidationExceptionOnPublishIfProcessedStatusIsNull -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/ProcessedStatusEvents/ProcessedStatusEventServiceTests.Validations.Publish.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Events.ProcessedStatuses;
+using Standardly.Core.Models.Events.ProcessedStatuses.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.ProcessedStatusEvents
+{
+    public partial class ProcessedStatusEventServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnPublishIfProcessedStatusIsNull()
+        {
+            // given
+            ProcessedStatus nullProcessedStatus = null;
+
+            var nullProcessedStatusException = new NullProcessedStatusException();
+
+            var expectedProcessedStatusEventValidationException =
+                new ProcessedStatusEventValidationException(nullProcessedStatusException);
+
+            ValueTask publishProcessedStatusTask =
+                this.processedStatusEventService.PublishProcessedStatusAsync(nullProcessedStatus);
+
+            ProcessedStatusEventValidationException actualProcessedStatusEventValidationException =
+                await Assert.ThrowsAsync<ProcessedStatusEventValidationException>(publishProcessedStatusTask.AsTask);
+
+            // when
+            actualProcessedStatusEventValidationException.Should()
+                .BeEquivalentTo(expectedProcessedStatusEventValidationException);
+
+            // then
+            this.eventBrokerMock.Verify(broker =>
+                broker.PublishProcessedEventAsync(nullProcessedStatus),
+                    Times.Never);
+
+            this.eventBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Standardly.Core/Models/Events/ProcessedStatuses/Exceptions/NullProcessedStatusException.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace Standardly.Core.Models.Events.ProcessedStatuses.Exceptions
+{
+    public class NullProcessedStatusException : Xeption
+    {
+        public NullProcessedStatusException()
+            : base(message: "Process status is null.")
+        { }
+    }
+}
