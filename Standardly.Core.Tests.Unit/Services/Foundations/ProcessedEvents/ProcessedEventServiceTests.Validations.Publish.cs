60032e69f39e112489be9ac141919d6ce90d72b8
Christo du Toit <christo@dutoit.co.uk>
2023-01-04 23:12:33 +0000

    ShouldThrowValidationExceptionOnPublishIfProcessedIsNull -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Foundations/ProcessedEvents/ProcessedEventServiceTests.Validations.Publish.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Foundations.ProcessedEvents;
+using Standardly.Core.Models.Foundations.ProcessedEvents.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Foundations.ProcessedEvents
+{
+    public partial class ProcessedEventServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnPublishIfProcessedIsNull()
+        {
+            // given
+            Processed nullProcessed = null;
+
+            var nullProcessedEventException = new NullProcessedEventException();
+
+            var expectedProcessedEventValidationException =
+                new ProcessedEventValidationException(nullProcessedEventException);
+
+            ValueTask publishProcessedTask =
+                this.processedEventService.PublishProcessedAsync(nullProcessed);
+
+            ProcessedEventValidationException actualProcessedEventValidationException =
+                await Assert.ThrowsAsync<ProcessedEventValidationException>(publishProcessedTask.AsTask);
+
+            // when
+            actualProcessedEventValidationException.Should()
+                .BeEquivalentTo(expectedProcessedEventValidationException);
+
+            // then
+            this.eventBrokerMock.Verify(broker =>
+                broker.PublishProcessedEventAsync(nullProcessed),
+                    Times.Never);
+
+            this.eventBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
