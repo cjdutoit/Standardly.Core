d2a678801c464c64f7c340911a1c1a07c089dd11
Christo du Toit <christo@dutoit.co.uk>
2023-01-05 16:47:45 +0000

    WIP

---
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/Operations/OperationOrchestrationServiceTests.cs
@@ -0,0 +1,12 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+namespace Standardly.Core.Tests.Unit.Services.Orchestrations.Operations
+{
+    public partial class OperationOrchestrationServiceTests
+    {
+    }
+}
---
+++ Standardly.Core/Services/Orchestrations/Operations/OperationOrchestrationService.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Standardly.Core.Services.Processings.Executions;
+using Standardly.Core.Services.Processings.Files;
+
+namespace Standardly.Core.Services.Orchestrations.Operations
+{
+    public partial class OperationOrchestrationService
+    {
+        private readonly IExecutionProcessingService executionProcessingService;
+        private readonly IFileProcessingService fileProcessingService;
+
+        public OperationOrchestrationService(
+            IExecutionProcessingService executionProcessingService,
+            IFileProcessingService fileProcessingService)
+        {
+            this.executionProcessingService = executionProcessingService;
+            this.fileProcessingService = fileProcessingService;
+        }
+    }
+}
