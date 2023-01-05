5465a8f47c5701080579897efe0cc41d4036d497
Christo du Toit <christo@dutoit.co.uk>
2023-01-05 14:25:40 +0000

    ShouldThrowDependencyValidationOnPublishIfDependencyValidationErrorOccurs -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Processings/ProcessedEvents/ProcessedEventProcessingServiceTests.Exceptions.Publish.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Foundations.ProcessedEvents;
+using Standardly.Core.Models.Processings.ProcessedEvents.Exceptions;
+using Xeptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.ProcessedEvents
+{
+    public partial class ProcessedEventProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationOnPublishIfDependencyValidationErrorOccurs(
+                    Xeption dependencyValidationException)
+        {
+            // given
+            Processed randomProcessed = CreateRandomProcessed();
+            Processed inputProcessed = randomProcessed;
+
+            var serviceException = new Exception();
+
+            var expectedProcessedEventProcessingDependencyValidationException =
+                new ProcessedEventProcessingDependencyValidationException(
+                    dependencyValidationException.InnerException as Xeption);
+
+            this.processedEventServiceMock.Setup(service =>
+                service.PublishProcessedAsync(inputProcessed))
+                    .Throws(dependencyValidationException);
+
+            ValueTask publishProcessedTask = this.processedEventProcessingService
+                .PublishProcessedAsync(inputProcessed);
+
+            ProcessedEventProcessingDependencyValidationException
+                actualProcessedEventProcessingDependencyValidationException =
+                    await Assert.ThrowsAsync<ProcessedEventProcessingDependencyValidationException>(
+                        publishProcessedTask.AsTask);
+
+            // when
+            actualProcessedEventProcessingDependencyValidationException.Should()
+                .BeEquivalentTo(expectedProcessedEventProcessingDependencyValidationException);
+
+            // then
+            this.processedEventServiceMock.Verify(service =>
+                service.PublishProcessedAsync(inputProcessed),
+                        Times.Once);
+
+            this.processedEventServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
