e02260b52e19029b9ca0a14ab6910e890fa9b97c
Christo du Toit <christo@dutoit.co.uk>
2023-01-05 00:02:57 +0000

    ShouldListenToProcessedEvents -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Processings/ProcessedEvents/ProcessedEventProcessingServiceTests.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Moq;
+using Standardly.Core.Services.Foundations.ProcessedEvents;
+using Standardly.Core.Services.Processings.ProcessedEvents;
+
+namespace Standardly.Core.Tests.Unit.Services.Processings.ProcessedEvents
+{
+    public partial class ProcessedEventProcessingServiceTests
+    {
+        private readonly Mock<IProcessedEventService> processedEventServiceMock;
+        private readonly IProcessedEventProcessingService processedEventProcessingService;
+
+        public ProcessedEventProcessingServiceTests()
+        {
+            this.processedEventServiceMock = new Mock<IProcessedEventService>();
+
+            this.processedEventProcessingService = new ProcessedEventProcessingService(
+                processedEventService: this.processedEventServiceMock.Object);
+        }
+    }
+}
