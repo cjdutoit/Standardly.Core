36a45075887c92325da56b2190fd314817e82665
Christo du Toit <christo@dutoit.co.uk>
2023-01-05 00:12:35 +0000

    ShouldThrowValidationExceptionOnListenToProcessedEventIfEventHandlerIsNull -> FAIL

---
+++ Standardly.Core/Models/Processings/ProcessedEvents/Exceptions/NullProcessedEventProcessingHandler.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace Standardly.Core.Models.Processings.ProcessedEvents.Exceptions
+{
+    public class NullProcessedEventProcessingHandler : Xeption
+    {
+        public NullProcessedEventProcessingHandler()
+            : base(message: "Processed event processing handler is null.")
+        { }
+    }
+}
