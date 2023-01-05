91e22ec6337bd862ece9b03b126268da488bbe00
Christo du Toit <christo@dutoit.co.uk>
2023-01-05 16:52:26 +0000

    CODE RUB:  Code cleanup and file rename

--- Standardly.Core.Tests.Unit/Services/Foundations/ProcessedEvents/ProcessedEventServiceTests.Exceptions.Listen.cs
+++ Standardly.Core.Tests.Unit/Services/Foundations/ProcessedEvents/ProcessedEventServiceTests.Exceptions.Listen.cs
@@ -47,9 +47,8 @@ namespace Standardly.Core.Tests.Unit.Services.Foundations.ProcessedEvents
 
             // then
             this.eventBrokerMock.Verify(broker =>
-                broker.ListenToProcessedEvent(
-                    processedEventHandlerMock.Object),
-                        Times.Once);
+                broker.ListenToProcessedEvent(processedEventHandlerMock.Object),
+                    Times.Once);
 
             this.eventBrokerMock.VerifyNoOtherCalls();
         }
