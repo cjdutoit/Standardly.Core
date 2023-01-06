07956afac43e9f107a6f7e226e2222dc0b7e4345
Christo du Toit <christo@dutoit.co.uk>
2023-01-06 22:32:57 +0000

    ShouldThrowValidationExceptionOnWriteToFileIfInputsIsInvalidAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/Operations/OperationOrchestrationServiceTests.Validations.WriteToFile.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Orchestrations.Operations.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Orchestrations.Operations
+{
+    public partial class OperationOrchestrationServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnWriteToFileIfInputsIsInvalidAsync(
+            string invalidInput)
+        {
+            // given
+            string invalidPath = invalidInput;
+            string invalidContent = invalidInput;
+
+            var invalidArgumentOperationOrchestrationException =
+                new InvalidArgumentOperationOrchestrationException();
+
+            invalidArgumentOperationOrchestrationException.AddData(
+                key: "path",
+                values: "Text is required");
+
+            invalidArgumentOperationOrchestrationException.AddData(
+                key: "content",
+                values: "Text is required");
+
+            var expectedOperationOrchestrationValidationException =
+                new OperationOrchestrationValidationException(invalidArgumentOperationOrchestrationException);
+
+            // when
+            ValueTask<bool> writeToFileTask =
+                this.operationOrchestrationService.WriteToFileAsync(path: invalidPath, content: invalidContent);
+
+            OperationOrchestrationValidationException actualException =
+                await Assert.ThrowsAsync<OperationOrchestrationValidationException>(writeToFileTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedOperationOrchestrationValidationException);
+
+            this.fileProcessingServiceMock.Verify(service =>
+                service.WriteToFileAsync(invalidPath, invalidContent),
+                    Times.Never);
+
+            this.fileProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
