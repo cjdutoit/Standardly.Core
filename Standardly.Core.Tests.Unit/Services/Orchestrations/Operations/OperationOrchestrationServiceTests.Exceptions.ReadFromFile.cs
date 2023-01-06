d70ed829c04ffa6635af4257b8cac779213341d4
Christo du Toit <christo@dutoit.co.uk>
2023-01-06 23:57:05 +0000

    ShouldThrowDependencyValidationOnReadFromFileAsyncIfDependencyValidationErrorOccursAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/Operations/OperationOrchestrationServiceTests.Exceptions.ReadFromFile.cs
@@ -0,0 +1,50 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Moq;
+using Standardly.Core.Models.Orchestrations.Operations.Exceptions;
+using Xeptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Orchestrations.Operations
+{
+    public partial class OperationOrchestrationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(FileDependencyValidationExceptions))]
+        public async Task
+            ShouldThrowDependencyValidationOnReadFromFileAsyncIfDependencyValidationErrorOccursAsync(
+                Xeption dependencyValidationException)
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputPath = randomPath;
+
+            var expectedOperationOrchestrationDependencyValidationException =
+                new OperationOrchestrationDependencyValidationException(
+                    dependencyValidationException.InnerException as Xeption);
+
+            this.fileProcessingServiceMock.Setup(service =>
+                service.ReadFromFileAsync(inputPath))
+                    .ThrowsAsync(dependencyValidationException);
+
+            // when
+            ValueTask<string> ReadFromFileTask =
+                this.operationOrchestrationService.ReadFromFileAsync(inputPath);
+
+            // then
+            OperationOrchestrationDependencyValidationException actualException =
+                await Assert.ThrowsAsync<OperationOrchestrationDependencyValidationException>(ReadFromFileTask.AsTask);
+
+            this.fileProcessingServiceMock.Verify(service =>
+                service.ReadFromFileAsync(inputPath),
+                    Times.Once);
+
+            this.fileProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
