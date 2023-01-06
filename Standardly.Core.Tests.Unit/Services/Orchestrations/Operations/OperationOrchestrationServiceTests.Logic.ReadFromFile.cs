528db29f5c844963e2d940a9b6545ea22925edfb
Christo du Toit <christo@dutoit.co.uk>
2023-01-06 23:14:48 +0000

    ShouldReadFromFileAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/Operations/OperationOrchestrationServiceTests.Logic.ReadFromFile.cs
@@ -0,0 +1,43 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Orchestrations.Operations
+{
+    public partial class OperationOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldReadFromFileAsync()
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputFilePath = randomPath;
+            string randomContent = GetRandomString();
+            string expectedResult = randomContent;
+
+            this.fileProcessingServiceMock.Setup(service =>
+                service.ReadFromFileAsync(randomPath))
+                    .ReturnsAsync(expectedResult);
+
+            // when
+            string actualResult =
+                await this.operationOrchestrationService.ReadFromFileAsync(inputFilePath);
+
+            // then
+            actualResult.Should().BeEquivalentTo(expectedResult);
+
+            this.fileProcessingServiceMock.Verify(service =>
+                service.ReadFromFileAsync(inputFilePath),
+                    Times.Once);
+
+            this.fileProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
