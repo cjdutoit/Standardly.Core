b731de0d00e31203d77a1d752b620dbeda257b76
Christo du Toit <christo@dutoit.co.uk>
2023-01-06 23:20:17 +0000

    ShouldThrowValidationExceptionOnReadFromFileIfPathIsInvalidAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/Operations/OperationOrchestrationServiceTests.Validations.ReadFromFile.cs
@@ -0,0 +1,52 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Orchestrations.Operations.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Orchestrations.Operations
+{
+    public partial class OperationOrchestrationServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnReadFromFileIfPathIsInvalidAsync(
+            string invalidFilePath)
+        {
+            // given
+            var invalidArgumentOperationOrchestrationException =
+                new InvalidArgumentOperationOrchestrationException();
+
+            invalidArgumentOperationOrchestrationException.AddData(
+                key: "path",
+                values: "Text is required");
+
+            var expectedFilesProcessingValidationException =
+                new OperationOrchestrationValidationException(invalidArgumentOperationOrchestrationException);
+
+            // when
+            ValueTask<string> ReadFromFileTask =
+                this.operationOrchestrationService.ReadFromFileAsync(invalidFilePath);
+
+            OperationOrchestrationValidationException actualException =
+                await Assert.ThrowsAsync<OperationOrchestrationValidationException>(ReadFromFileTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFilesProcessingValidationException);
+
+            this.fileProcessingServiceMock.Verify(service =>
+                service.ReadFromFileAsync(invalidFilePath),
+                    Times.Never);
+
+            this.fileProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
