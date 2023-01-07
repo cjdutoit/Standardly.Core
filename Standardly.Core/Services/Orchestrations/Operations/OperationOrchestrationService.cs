1922ee35dad74f2b1080c2d0886df560d2ccbdf1
Christo du Toit <christo@dutoit.co.uk>
2023-01-07 13:10:49 +0000

    ShouldCreateDirectoryAsync -> PASS

--- Standardly.Core/Services/Orchestrations/Operations/OperationOrchestrationService.cs
+++ Standardly.Core/Services/Orchestrations/Operations/OperationOrchestrationService.cs
@@ -92,8 +92,8 @@ namespace Standardly.Core.Services.Orchestrations.Operations
                 return await this.fileProcessingService.CheckIfDirectoryExistsAsync(path);
             });
 
-        public ValueTask<bool> CreateDirectoryAsync(string path) =>
-            throw new NotImplementedException();
+        public async ValueTask<bool> CreateDirectoryAsync(string path) =>
+            await this.fileProcessingService.CreateDirectoryAsync(path);
 
         public ValueTask<bool> DeleteDirectoryAsync(string path, bool recursive = false) =>
             throw new NotImplementedException();
