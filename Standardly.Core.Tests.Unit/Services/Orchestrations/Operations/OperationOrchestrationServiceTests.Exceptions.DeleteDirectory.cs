7d5f635a86879e894616e5558bb99605ae7bc169
Christo du Toit <christo@dutoit.co.uk>
2023-01-07 14:06:16 +0000

    ShouldThrowDependencyValidationOnDeleteDirectoryIfDependencyValidationErrorOccursAsync -> PASS

---
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/Operations/OperationOrchestrationServiceTests.Exceptions.DeleteDirectory.cs
@@ -0,0 +1,51 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Moq;
+using Standardly.Core.Models.Orchestrations.Operations.Exceptions;
+using Xeptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Orchestrations.Operations
+{
+    public partial class OperationOrchestrationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(FileDependencyValidationExceptions))]
+        public async Task
+            ShouldThrowDependencyValidationOnDeleteDirectoryIfDependencyValidationErrorOccursAsync(
+                Xeption dependencyValidationException)
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputPath = randomPath;
+            bool recursive = true;
+
+            var expectedOperationOrchestrationDependencyValidationException =
+                new OperationOrchestrationDependencyValidationException(
+                    dependencyValidationException.InnerException as Xeption);
+
+            this.fileProcessingServiceMock.Setup(service =>
+                service.DeleteDirectoryAsync(inputPath, recursive))
+                    .ThrowsAsync(dependencyValidationException);
+
+            // when
+            ValueTask<bool> deleteDirectoryTask =
+                this.operationOrchestrationService.DeleteDirectoryAsync(inputPath, recursive);
+
+            // then
+            OperationOrchestrationDependencyValidationException actualException =
+                await Assert.ThrowsAsync<OperationOrchestrationDependencyValidationException>(deleteDirectoryTask.AsTask);
+
+            this.fileProcessingServiceMock.Verify(service =>
+                service.DeleteDirectoryAsync(inputPath, recursive),
+                    Times.Once);
+
+            this.fileProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
