dfe9a432ffba5ab9896e1978805c3a3749ec1b23
Christo du Toit <christo@dutoit.co.uk>
2023-01-07 12:48:31 +0000

    ShouldThrowDependencyValidationOnCheckIfDirectoryExistsIfDependencyValidationErrorOccursAsync -> PASS

---
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/Operations/OperationOrchestrationServiceTests.Exceptions.CheckIfDirectoryExists.cs
@@ -0,0 +1,51 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Moq;
+using Standardly.Core.Models.Orchestrations.Operations.Exceptions;
+using Xeptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Orchestrations.Operations
+{
+    public partial class OperationOrchestrationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(FileDependencyValidationExceptions))]
+        public async Task
+            ShouldThrowDependencyValidationOnCheckIfDirectoryExistsIfDependencyValidationErrorOccursAsync(
+                Xeption dependencyValidationException)
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputPath = randomPath;
+
+            var expectedOperationOrchestrationDependencyValidationException =
+                new OperationOrchestrationDependencyValidationException(
+                    dependencyValidationException.InnerException as Xeption);
+
+            this.fileProcessingServiceMock.Setup(service =>
+                service.CheckIfDirectoryExistsAsync(inputPath))
+                    .ThrowsAsync(dependencyValidationException);
+
+            // when
+            ValueTask<bool> checkIfDirectoryExistsTask =
+                this.operationOrchestrationService.CheckIfDirectoryExistsAsync(inputPath);
+
+            // then
+            OperationOrchestrationDependencyValidationException actualException =
+                await Assert.ThrowsAsync<OperationOrchestrationDependencyValidationException>(
+                    checkIfDirectoryExistsTask.AsTask);
+
+            this.fileProcessingServiceMock.Verify(service =>
+                service.CheckIfDirectoryExistsAsync(inputPath),
+                    Times.Once);
+
+            this.fileProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
