414cf38745cd174279433f77417088e750ce1f68
Christo du Toit <christo@dutoit.co.uk>
2023-01-07 01:09:58 +0000

    ShouldDeleteFileAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/Operations/OperationOrchestrationServiceTests.Logic.DeleteFile.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Orchestrations.Operations
+{
+    public partial class OperationOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldDeleteFileAsync()
+        {
+            // given
+            string randomPath = GetRandomString();
+            string inputFilePath = randomPath;
+            string randomContent = GetRandomString();
+            string inputContent = randomContent;
+
+            // when
+            await this.operationOrchestrationService.DeleteFileAsync(inputFilePath);
+
+            // then
+            this.fileProcessingServiceMock.Verify(service =>
+                service.DeleteFileAsync(inputFilePath),
+                    Times.Once);
+
+            this.fileProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
