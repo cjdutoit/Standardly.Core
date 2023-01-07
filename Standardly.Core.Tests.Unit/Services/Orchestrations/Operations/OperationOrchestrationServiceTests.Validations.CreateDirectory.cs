06293a17c4cf39a831cadc12bf2a053bb5cc24c8
Christo du Toit <christo@dutoit.co.uk>
2023-01-07 13:16:17 +0000

    ShouldThrowValidationExceptionOnCreateDirectoryIfInputsIsInvalidAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/Operations/OperationOrchestrationServiceTests.Validations.CreateDirectory.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Orchestrations.Operations.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Orchestrations.Operations
+{
+    public partial class OperationOrchestrationServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnCreateDirectoryIfInputsIsInvalidAsync(
+            string invalidInput)
+        {
+            // given
+            string invalidPath = invalidInput;
+
+            var invalidArgumentOperationOrchestrationException =
+                new InvalidArgumentOperationOrchestrationException();
+
+            invalidArgumentOperationOrchestrationException.AddData(
+                key: "path",
+                values: "Text is required");
+
+            var expectedOperationOrchestrationValidationException =
+                new OperationOrchestrationValidationException(invalidArgumentOperationOrchestrationException);
+
+            // when
+            ValueTask<bool> createDirectoryTask =
+                this.operationOrchestrationService.CreateDirectoryAsync(path: invalidPath);
+
+            OperationOrchestrationValidationException actualException =
+                await Assert.ThrowsAsync<OperationOrchestrationValidationException>(createDirectoryTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedOperationOrchestrationValidationException);
+
+            this.fileProcessingServiceMock.Verify(service =>
+                service.CreateDirectoryAsync(invalidPath),
+                    Times.Never);
+
+            this.fileProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
