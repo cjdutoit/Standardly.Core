1e46c50b154dcc7e65b9749f8f6dfa04a1cf7838
Christo du Toit <christo@dutoit.co.uk>
2023-01-07 01:19:37 +0000

    ShouldThrowValidationExceptionOnDeleteFileIfInputsIsInvalidAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/Operations/OperationOrchestrationServiceTests.Validations.DeleteFile.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Orchestrations.Operations.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Orchestrations.Operations
+{
+    public partial class OperationOrchestrationServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnDeleteFileIfInputsIsInvalidAsync(
+            string invalidInput)
+        {
+            // given
+            string invalidPath = invalidInput;
+
+            var invalidArgumentOperationOrchestrationException =
+                new InvalidArgumentOperationOrchestrationException();
+
+            invalidArgumentOperationOrchestrationException.AddData(
+                key: "path",
+                values: "Text is required");
+
+            var expectedFilesProcessingValidationException =
+                new OperationOrchestrationValidationException(invalidArgumentOperationOrchestrationException);
+
+            // when
+            ValueTask<bool> deleteFileTask =
+                this.operationOrchestrationService.DeleteFileAsync(path: invalidPath);
+
+            OperationOrchestrationValidationException actualException =
+                await Assert.ThrowsAsync<OperationOrchestrationValidationException>(deleteFileTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFilesProcessingValidationException);
+
+            this.fileProcessingServiceMock.Verify(service =>
+                service.DeleteFileAsync(invalidPath),
+                    Times.Never);
+
+            this.fileProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
