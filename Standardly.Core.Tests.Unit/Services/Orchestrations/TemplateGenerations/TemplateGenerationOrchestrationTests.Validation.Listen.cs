2680d9be875686376e54b18add2091f61ca2c45e
Christo du Toit <christo@dutoit.co.uk>
2023-01-10 22:37:19 +0000

    ShouldThrowValidationExceptionOnListenToProcessedEventIfEventHandlerIsNull -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/TemplateGenerations/TemplateGenerationOrchestrationTests.Validation.Listen.cs
@@ -0,0 +1,52 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Services.Foundations.ProcessedEvents;
+using Standardly.Core.Models.Services.Orchestrations.TemplateGenerations;
+using Standardly.Core.Models.Services.Orchestrations.TemplateGenerations.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Orchestrations.TemplateGenerations
+{
+    public partial class TemplateGenerationOrchestrationTests
+    {
+        [Fact]
+        public void ShouldThrowValidationExceptionOnListenToProcessedEventIfEventHandlerIsNull()
+        {
+            // given
+            var processedOrchestrationEventHandlerMock =
+                new Mock<Func<TemplateGenerationInfo, ValueTask<TemplateGenerationInfo>>>();
+
+            var nullProcessedEventOrchestrationHandler =
+                new NullProcessedEventOrchestrationHandlerException();
+
+            var expectedProcessedEventOrchestrationValidationException =
+                new ProcessedEventOrchestrationValidationException(nullProcessedEventOrchestrationHandler);
+
+            Action listenToProcessedEventAction = () => this.templateGenerationOrchestrationService
+                .ListenToProcessedEvent(processedOrchestrationEventHandlerMock.Object);
+
+            ProcessedEventOrchestrationValidationException actualProcessedEventOrchestrationValidationException =
+                Assert.Throws<ProcessedEventOrchestrationValidationException>(listenToProcessedEventAction);
+
+            // when
+            actualProcessedEventOrchestrationValidationException.Should()
+                .BeEquivalentTo(expectedProcessedEventOrchestrationValidationException);
+
+            // then
+            this.processedEventProcessingServiceMock.Verify(service =>
+                service.ListenToProcessedEvent(It.IsAny<Func<Processed, ValueTask<Processed>>>()),
+                    Times.Never);
+
+            this.processedEventProcessingServiceMock.VerifyNoOtherCalls();
+            this.templateProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
