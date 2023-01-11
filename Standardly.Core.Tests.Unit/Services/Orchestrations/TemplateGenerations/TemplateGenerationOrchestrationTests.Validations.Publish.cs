9000421dd8e136a382d54f934c06df6c875e3a0a
Christo du Toit <christo@dutoit.co.uk>
2023-01-11 12:16:40 +0000

    ShouldThrowValidationExceptionOnPublishIfProcessedIsNullAsync -> FAIL

---
+++ Standardly.Core.Tests.Unit/Services/Orchestrations/TemplateGenerations/TemplateGenerationOrchestrationTests.Validations.Publish.cs
@@ -0,0 +1,49 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standardly.Core.Models.Services.Orchestrations.TemplateGenerations;
+using Standardly.Core.Models.Services.Orchestrations.TemplateGenerations.Exceptions;
+using Xunit;
+
+namespace Standardly.Core.Tests.Unit.Services.Orchestrations.TemplateGenerations
+{
+    public partial class TemplateGenerationOrchestrationTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnPublishIfProcessedIsNullAsync()
+        {
+            // given
+            TemplateGenerationInfo nullTemplateGenerationInfo = null;
+
+            var nullTemplateGenerationInfoOrchestrationException =
+                new NullTemplateGenerationInfoOrchestrationException();
+
+            var expectedProcessedEventOrchestrationValidationException =
+                new ProcessedEventOrchestrationValidationException(nullTemplateGenerationInfoOrchestrationException);
+
+            // when
+            ValueTask publishProcessedTask =
+                this.templateGenerationOrchestrationService.PublishProcessedAsync(nullTemplateGenerationInfo);
+
+            ProcessedEventOrchestrationValidationException actualProcessedEventOrchestrationValidationException =
+                await Assert.ThrowsAsync<ProcessedEventOrchestrationValidationException>(publishProcessedTask.AsTask);
+
+            // then
+            actualProcessedEventOrchestrationValidationException.Should()
+                .BeEquivalentTo(expectedProcessedEventOrchestrationValidationException);
+
+            this.processedEventProcessingServiceMock.Verify(service =>
+                service.PublishProcessedAsync(nullTemplateGenerationInfo.Processed),
+                    Times.Never);
+
+            this.processedEventProcessingServiceMock.VerifyNoOtherCalls();
+            this.templateProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
